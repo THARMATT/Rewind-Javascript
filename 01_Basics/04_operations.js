d039464779b564ef0e21f0df02cfd5ce07831463
Nigam Sharma <132887723+THARMATT@users.noreply.github.com>
2023-10-03 07:12:31 +0000

    baics

---
+++ 01_Basics/04_operations.js
@@ -0,0 +1,6 @@
+let val =3;
+let negval=-val;
+console.log(negval)
+//basic operations like * / + - %
+console.log(1+2+"3")// ans 33
+console.log("1"+2+2)//ans 122
---
+++ 01_Basics/05_comparisions.js
@@ -0,0 +1,14 @@
+//> < += == !=
+console.log("2">1);
+console.log(null==0);
+console.log(null>=0);
+console.log(undefined==0);
+
+/*******************************88888888888888888888 */
+//reference (Non Primitive)
+//Array, objects, functions
+//+++++++++++++++++++++++++++++++++
+
+//STACK AND heap
+//primitive - stack :copy
+//not primitive - heap :reference
