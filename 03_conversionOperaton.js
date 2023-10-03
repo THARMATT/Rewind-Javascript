fca3698a6a9ed3ae850b8ce2e783f6a0df04a21c
Nigam Sharma <132887723+THARMATT@users.noreply.github.com>
2023-10-03 06:13:24 +0000

    conversionjs

---
+++ 03_conversionOperaton.js
@@ -0,0 +1,10 @@
+let score="33"
+console.log(typeof (score))
+console.log(typeof score)
+ let valNum=Number(score)
+ console.log(typeof valNum)
+//null -0
+//string-Nan
+//33-number
+//33q-Nan
+//true-1//false-0
