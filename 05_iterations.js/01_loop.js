126ecac568dffc721383c288cbe9f73ea5ad251b
Nigam Sharma <132887723+THARMATT@users.noreply.github.com>
2023-10-06 04:47:22 +0000

    loops

---
+++ 05_iterations.js/01_loop.js
@@ -0,0 +1,33 @@
+//for 
+ 
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    // console.log(element);
+}
+
+
+for (let i = 1; i < 3; i++) {
+    console.log(`Table for ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        
+        // console.log(`inner-loop ${index} and outeris ${i}`);
+        console.log(`${i} X ${j}=${i*j}`);
+    }
+    
+}
+
+let array=['batman','aquaman','flash']
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    // console.log(element);
+}
+
+//break and continue
+for (let index = 1; index < 20; index++) {
+
+    if(index==5){
+        
+    }
+    console.log(`Value of i is : ${index}`);
+    
+}
